f1e3d69b433b45d9735d9ba8dce74e58e2cdaca6
Kateryna Mushynska <kate080227@gmail.com>
2022-03-21 21:37:34 +0200

    kmushynska: added Google test to verify search

---
+++ src/test/java/com/softserveinc/ita/kmushynska/GoogleTest.java
@@ -0,0 +1,30 @@
+package com.softserveinc.ita.kmushynska;
+
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.Keys.ENTER;
+
+public class GoogleTest {
+
+    @Test
+    public void verifyThatGoogleHomePageIsOpen() {
+        Configuration.browser = "Chrome";
+        Configuration.timeout = 20 * 1000;
+        open("https://google.com");
+        webdriver().driver().getWebDriver().manage().window().maximize();
+
+        String searchTerm = "funny dogs";
+        String inputFieldPath = "//input[@class='gLFyf gsfi']";
+        $x(inputFieldPath).sendKeys(searchTerm);
+        $x(inputFieldPath).sendKeys(ENTER);
+
+        $x("//div[@class='logo']").click();
+
+        $x("//title")
+                .shouldHave(attribute("text", "Google"));
+
+    }
+}
