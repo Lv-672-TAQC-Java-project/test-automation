beb53d198c18fa0db1f67311e5633160636e5764
Taras Oleksyn <taras.oleksyn@gmail.com>
2022-03-21 09:24:18 +0200

    toleksyn: initial commit

--- src/test/java/com/softserveinc/ita/toleksyn/GoogleTest.java
+++ src/test/java/com/softserveinc/ita/toleksyn/GoogleTest.java
@@ -1,5 +1,36 @@
 package com.softserveinc.ita.toleksyn;
 
+import com.codeborne.selenide.Configuration;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+import static java.time.Duration.ofSeconds;
+import static org.openqa.selenium.Keys.ENTER;
+
+/*
+This is a simple Selenide usage example, to be deleted
+ */
 public class GoogleTest {
-    // sample class
+
+    @Test
+    public void verifyThatSearchingDisplaysResults() {
+        // open google home page
+        // search for 'funny kitten'
+        // verify that first link contains word 'kitten'
+        Configuration.browser = "chrome";
+        Configuration.timeout = 30 * 1000;
+        open("https://google.com");
+
+        String searchTerm = "funny kitten";
+        String inputFieldPath = "//input[@class='gLFyf gsfi']";
+        $x(inputFieldPath).sendKeys(searchTerm);
+        $x(inputFieldPath).sendKeys(ENTER);
+
+        $x("(//a/h3)[1]")
+                .shouldBe(visible, enabled)
+                .shouldHave(text(searchTerm)
+                        .because("Second link text should contain " + searchTerm), ofSeconds(4));
+    }
 }
