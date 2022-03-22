d1a95149242e306186c3e29c5eb5f9d9610ab739
OlehPysko <pysko.oleh@gmail.com>
2022-03-22 18:26:33 +0200

    OlehPysko: added Google test to verify search

--- src/test/java/com/softserveinc/ita/OlehPysko/GoogleTest.java
+++ src/test/java/com/softserveinc/ita/OlehPysko/GoogleTest.java
@@ -2,27 +2,23 @@ package com.softserveinc.ita.OlehPysko;
 
 import com.codeborne.selenide.*;
 import org.testng.annotations.Test;
-import java.time.Duration;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$x;
 import static org.openqa.selenium.Keys.ENTER;
-
 
 public class GoogleTest {
     @Test
     public void verifyThatNextAndPreviousLinkIsDisplayed(){
-
         Configuration.browser = "chrome";
         Selenide.open("https://google.com");
 
         String searchTerm = "funny dogs";
         String inputFieldPath = "//input[@class='gLFyf gsfi']";
-
         $x(inputFieldPath).sendKeys(searchTerm);
         $x(inputFieldPath).sendKeys(ENTER);
 
-        $x("//span[contains(text(),'Next')]").isDisplayed();
+        $x("(//a[@id='pnnext']/span)[2]").shouldBe(visible.because("link Next is displayed"));
         $x("//a[@aria-label = 'Page 4']").click();
-        $x("//span[contains(text(),'Previous')]").isDisplayed();
-
+        $x("(//a[@id='pnprev']/span)[2]").shouldBe(visible.because("link Previous is displayed"));
     }
 }
