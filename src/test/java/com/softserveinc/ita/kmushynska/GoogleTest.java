6cf80fa0b1f9eaca8ed535581b7f17d68ddffa51
Kateryna Mushynska <kate080227@gmail.com>
2022-03-23 20:05:05 +0200

    kmushynska: added additional verifications and refactoring Google test

--- src/test/java/com/softserveinc/ita/kmushynska/GoogleTest.java
+++ src/test/java/com/softserveinc/ita/kmushynska/GoogleTest.java
@@ -1,22 +1,16 @@
 package com.softserveinc.ita.kmushynska;
 
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.WebDriverRunner;
+import com.softserveinc.ita.TestRunner;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.Keys.ENTER;
 
-public class GoogleTest {
+public class GoogleTest extends TestRunner {
 
     @Test
     public void verifyThatGoogleHomePageIsOpen() {
-        Configuration.browser = "Chrome";
-        Configuration.timeout = 20 * 1000;
-        open("https://google.com");
-        WebDriverRunner.getWebDriver().manage().window().maximize();
-
         String searchTerm = "funny dogs";
         String inputFieldPath = "//input[@class='gLFyf gsfi']";
         $x(inputFieldPath).sendKeys(searchTerm);
@@ -24,7 +18,16 @@ public class GoogleTest {
 
         $x("//div[@class='logo']").click();
 
-        $x("//title")
-                .shouldHave(attribute("text", "Google"));
+        String message = "Google home page is opened";
+        $x("//img[@class='lnXdpd']")
+                .shouldBe(visible.because(message));
+        $x("//div[@class='RNNXgb']")
+                .shouldBe(visible.because(message));
+        $x("(//input[@class='gNO89b'])[2]")
+                .shouldBe(visible.because(message));
+        $x("(//input[@class='RNmpXc'])[2]")
+                .shouldBe(visible.because(message));
+        $x("//div[@id='SIvCob']")
+                .shouldBe(visible.because(message));
     }
 }
