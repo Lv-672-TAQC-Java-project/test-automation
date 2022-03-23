b31eb5c73d4ec0b040acebe99f376ec6a10275b1
Inna <innapopenko45@gmail.com>
2022-03-23 09:25:41 +0200

    ipopenko: fix comments

--- src/main/java/TestRunner.java
+++ src/main/java/TestRunner.java
@@ -1,15 +1,18 @@
-package com.softserveinc.ita;
-
 import com.codeborne.selenide.Configuration;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestRunner {
-    @BeforeMethod
-    public void initDriver() {
+    @BeforeSuite
+    public void driverConfigurationAndTimeout() {
         Configuration.browser = "chrome";
-        Configuration.timeout = 10 * 1000;
+        Configuration.timeout = 60 * 1000;
+    }
+
+    @BeforeMethod
+    public void navigateToUrl() {
         open("https://google.com");
     }
 }
