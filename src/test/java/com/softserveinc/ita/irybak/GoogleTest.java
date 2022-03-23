d97a88507230c5d48f21df8c0c707478cf768814
Ira Rybak <ira.rybak17@gmail.com>
2022-03-23 20:35:30 +0200

    irybak: fixed Google test and added extend from TestRunner

--- src/test/java/com/softserveinc/ita/irybak/GoogleTest.java
+++ src/test/java/com/softserveinc/ita/irybak/GoogleTest.java
@@ -1,20 +1,15 @@
 package com.softserveinc.ita.irybak;
 
-import com.codeborne.selenide.Configuration;
+import com.softserveinc.ita.TestRunner;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
-import static com.codeborne.selenide.Selenide.open;
 import static org.openqa.selenium.Keys.ENTER;
 
-public class GoogleTest {
+public class GoogleTest extends TestRunner {
     @Test
-    public void verifyThatNextLinkIsDisplayed(){
-        Configuration.browser = "chrome";
-        Configuration.timeout = 30 * 1000;
-        open("https://google.com");
-
+    public void verifyThatNextLinkIsDisplayed() {
         String searchTerm = "funny dogs";
         String inputFieldPath = "//input[@name = 'q']";
         $x(inputFieldPath).sendKeys(searchTerm);
