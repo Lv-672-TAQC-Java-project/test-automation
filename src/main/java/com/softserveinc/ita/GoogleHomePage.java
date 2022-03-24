2fb923f4afd8280b7ceb0c687a7dfbc3fa90f41a
Inna <innapopenko45@gmail.com>
2022-03-24 11:43:37 +0200

    ipopenko: added GoogleHomePage

---
+++ src/main/java/com/softserveinc/ita/GoogleHomePage.java
@@ -0,0 +1,22 @@
+package com.softserveinc.ita;
+
+import com.codeborne.selenide.Selenide;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static org.openqa.selenium.Keys.ENTER;
+
+public class GoogleHomePage {
+    public GoogleHomePage open(String url) {
+        Selenide.open(url);
+
+        return this;
+    }
+
+    public GoogleSearchResultPage search(String term) {
+        String inputFieldPath = "//input[@name='q']";
+        $x(inputFieldPath).sendKeys(term);
+        $x(inputFieldPath).sendKeys(ENTER);
+
+        return new GoogleSearchResultPage();
+    }
+}
