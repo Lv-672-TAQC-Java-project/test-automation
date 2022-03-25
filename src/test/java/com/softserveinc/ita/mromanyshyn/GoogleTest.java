e98d6863b69276a430deb5d8164ec2e9b4b7daae
Michael Romanyshyn <mikesunry@gmail.com>
2022-03-25 10:40:34 +0200

    mromanyshyn: edited GoogleTest class

--- src/test/java/com/softserveinc/ita/mromanyshyn/GoogleTest.java
+++ src/test/java/com/softserveinc/ita/mromanyshyn/GoogleTest.java
@@ -5,42 +5,13 @@ import com.softserveinc.ita.GoogleSearchResultPage;
 import com.softserveinc.ita.TestRunner;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.*;
-import static java.time.Duration.ofSeconds;
-import static org.openqa.selenium.Keys.ENTER;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.*;
 
 public class GoogleTest extends TestRunner {
 
     @Test
-    public void verifyThatFirstLinkContainsKittenNotDogs() {
-        String inputFieldPath = "//input[@class='gLFyf gsfi']";
-        String searchFirstTerm = "funny dogs";
-
-        $x(inputFieldPath).sendKeys(searchFirstTerm);
-        $x(inputFieldPath).sendKeys(ENTER);
-        $x(inputFieldPath).clear();
-
-        String searchSecondTerm = "funny kitten";
-
-        $x(inputFieldPath).sendKeys(searchSecondTerm);
-        $x(inputFieldPath).sendKeys(ENTER);
-
-        String expectedTerm = "kitten";
-        String firstLinkPath = "(//div[@class = 'g dFd2Tb']) [1]";
-
-        $x(firstLinkPath)
-                .shouldBe(visible, enabled)
-                .shouldNotHave(text("dogs"))
-                .shouldHave(text(expectedTerm)
-                        .because("First link text should contain " + expectedTerm), ofSeconds(5));
-    }
-
-    @Test
     public void verifyThatFirstLinkContainsKittenNotDogs_PO() {
-        GoogleHomePage googleHomePage = new GoogleHomePage().open("https://www.google.com/");
+        GoogleHomePage googleHomePage = new GoogleHomePage().open();
         String firstTerm = "funny dogs";
         String secondTerm = "funny kitten";
 
