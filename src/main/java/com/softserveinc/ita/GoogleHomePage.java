dfcfbb586bc50466b2f393642d3cf90c8bdb7e22
Kateryna Mushynska <kate080227@gmail.com>
2022-03-29 08:11:24 +0300

    kmushynska: fixed GoogleHomePage

--- src/main/java/com/softserveinc/ita/GoogleHomePage.java
+++ src/main/java/com/softserveinc/ita/GoogleHomePage.java
@@ -2,6 +2,7 @@ package com.softserveinc.ita;
 
 import com.codeborne.selenide.Selenide;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 import static org.openqa.selenium.Keys.ENTER;
 
@@ -23,30 +24,30 @@ public class GoogleHomePage {
     public boolean isGoogleLogoDisplayed() {
         String googleLogoPath = "//img[@class='lnXdpd']";
 
-        return $x(googleLogoPath).isDisplayed();
+        return $x(googleLogoPath).shouldBe(visible).isDisplayed();
     }
 
     public boolean isSearchInputFieldDisplayed() {
         String searchInputFieldPath = "//div[@class='RNNXgb']";
 
-        return $x(searchInputFieldPath).isDisplayed();
+        return $x(searchInputFieldPath).shouldBe(visible).isDisplayed();
     }
 
     public boolean isButtonGoogleSearchDisplayed() {
         String buttonGoogleSearchPath = "(//input[@class='gNO89b'])[2]";
 
-        return $x(buttonGoogleSearchPath).isDisplayed();
+        return $x(buttonGoogleSearchPath).shouldBe(visible).isDisplayed();
     }
 
     public boolean isButtonImFeelingLuckyDisplayed() {
         String buttonImFeelingLuckyPath = "(//input[@class='RNmpXc'])[2]";
 
-        return $x(buttonImFeelingLuckyPath).isDisplayed();
+        return $x(buttonImFeelingLuckyPath).shouldBe(visible).isDisplayed();
     }
 
     public boolean isGoogleOfferedInElementDisplayed() {
         String googleOfferedInPath = "//div[@id='SIvCob']";
 
-        return $x(googleOfferedInPath).isDisplayed();
+        return $x(googleOfferedInPath).shouldBe(visible).isDisplayed();
     }
 }
