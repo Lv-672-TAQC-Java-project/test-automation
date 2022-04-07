5ddea7868ecc1f7b9ea0f9657483d6f00f579af2
OlehPysko <pysko.oleh@gmail.com>
2022-04-07 23:57:35 +0300

    OlehPysko: added test to verify delete function in comparison list

--- src/main/java/com/softserveinc/ita/ComparisonModal.java
+++ src/main/java/com/softserveinc/ita/ComparisonModal.java
@@ -1,5 +1,50 @@
 package com.softserveinc.ita;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Selenide.*;
+
 public class ComparisonModal {
-    // to be extended
+
+    public boolean isVisibleProduct() {
+        sleep(3000);
+        for (SelenideElement element : $$x("//a[@class='product__heading']"))
+//    return element.should(Condition.visible, Duration.ofSeconds(3)).isDisplayed();
+            return element.should(Condition.visible).isDisplayed();
+
+        return false;
+    }
+
+
+    @Step("count products in comparison modal")
+    public ElementsCollection countProduct(int amountProduct) {
+
+        return $$x("//a[@class='product__heading']").shouldHave(size(amountProduct));
+    }
+
+    @Step("deleted products from comparison modal")
+    public ComparisonModal deleteElement(int countElement) {
+        sleep(2000);
+
+//        for (int i = 0; i < countElement; i++){
+////            sleep(2000);
+//            $$x("//button[@class='button button--white button--small popup-menu__toggle popup-menu__toggle--context']").get(i).click();
+//        $x("//button[@class='button button--medium button--with-icon button--link context-menu-actions__button']").click();
+//        }
+
+        for (int i = 1; i <= countElement; i++) {
+            String linkText = String.format("//li[@class='products-grid__cell ng-star-inserted'][%s]" +
+                    "//button[@class='button button--white button--small popup-menu__toggle popup-menu__toggle--context']", i);
+            $x(linkText).click();
+//            sleep(2000);
+            $x("//button[@class='button button--medium button--with-icon button--link context-menu-actions__button']").click();
+        }
+        return this;
+    }
 }
--- src/main/java/com/softserveinc/ita/HomePage.java
+++ src/main/java/com/softserveinc/ita/HomePage.java
@@ -3,6 +3,8 @@ package com.softserveinc.ita;
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.Selenide.$x;
+
 public class HomePage {
 
     private Header header = new Header();
@@ -13,4 +15,14 @@ public class HomePage {
 
         return this;
     }
+
+    @Step("Opened product page from Menu")
+    public MenuCategories openLinkInMenu(String linkMenu){
+        String linkText = String.format("//ul[@class='menu-categories menu-categories_type_main']" +
+                "/li[@class='menu-categories__item ng-star-inserted']/a[contains(text(),'%s')]", linkMenu);
+        $x(linkText).click();
+
+        return new MenuCategories();
+    }
+
 }
