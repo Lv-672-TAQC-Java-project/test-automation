c513fbce03e498b0805644ea4fb2f18e3ed56556
yurii <yuriidru23@gmail.com>
2022-04-10 12:21:21 +0300

    ydruzhytskyi: added getSearchedProductItemName method

--- src/main/java/com/softserveinc/ita/SearchResultPage.java
+++ src/main/java/com/softserveinc/ita/SearchResultPage.java
@@ -1,6 +1,12 @@
 package com.softserveinc.ita;
 
+import static com.codeborne.selenide.Selenide.$x;
+
 public class SearchResultPage {
 
     private Header header = new Header();
+
+    public String getSearchedProductItemName() {
+        return $x("//div[@class='search-header ng-star-inserted']/h1").getText();
+    }
 }
