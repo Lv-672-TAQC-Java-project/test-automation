a2610f17d96e18f4fcf2dd7671d11685f360afa3
Inna <innapopenko45@gmail.com>
2022-04-10 11:39:16 +0300

    ipopenko: added class Product and method getProducts

---
+++ src/main/java/com/softserveinc/ita/Product.java
@@ -0,0 +1,39 @@
+package com.softserveinc.ita;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class Product {
+
+    private final String rootElementPath;
+
+    public Product(String element) {
+        this.rootElementPath = element;
+    }
+
+    public String getProductName() {
+
+        return $x(String.format("%s%s", rootElementPath,
+                "//span[@class = 'goods-tile__title']")).text();
+    }
+
+    public String getPriceProduct() {
+
+        return $x(String.format("%s%s", rootElementPath,
+                "//span[@class = 'goods-tile__price-value']")).text();
+    }
+
+    public SearchResultPage addToCartProduct() {
+        $x(String.format("%s%s", rootElementPath,
+                "//button[contains(@class, 'goods-tile__buy-button')]")).click();
+
+        return new SearchResultPage();
+    }
+
+    public SearchResultPage addProductToListOfComparisons() {
+        $x(String.format("%s%s", rootElementPath,
+                "//button[contains(@class, 'compare-button')]")).click();
+
+        return new SearchResultPage();
+    }
+}
+
--- src/main/java/com/softserveinc/ita/SearchResultPage.java
+++ src/main/java/com/softserveinc/ita/SearchResultPage.java
@@ -1,6 +1,26 @@
 package com.softserveinc.ita;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.codeborne.selenide.CollectionCondition.sizeNotEqual;
+import static com.codeborne.selenide.Selenide.$$x;
+
 public class SearchResultPage {
 
     private Header header = new Header();
+
+    public List<Product> getProducts() {
+        List<Product> products = new ArrayList<>();
+        String productsPath = "//div[@class='goods-tile__inner']";
+        int amountOfProducts = $$x(productsPath)
+                .shouldHave(sizeNotEqual(0), Duration.ofSeconds(5)).size();
+
+        for (int i = 1; i <= amountOfProducts; i++) {
+            products.add(new Product(String.format("(%s)[%s]", productsPath, i)));
+        }
+
+        return products;
+    }
 }
