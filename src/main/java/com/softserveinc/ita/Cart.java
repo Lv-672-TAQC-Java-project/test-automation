9e35a2b9cc32b0953934a18f2d693c48fe752312
yurii <yuriidru23@gmail.com>
2022-04-11 16:09:04 +0300

    ydruzhytskyi: added requested code changes

--- src/main/java/com/softserveinc/ita/Cart.java
+++ src/main/java/com/softserveinc/ita/Cart.java
@@ -1,10 +1,11 @@
 package com.softserveinc.ita;
 
 import static com.codeborne.selenide.Selenide.$x;
+import static java.lang.String.format;
 
 public class Cart {
 
-    public String getFirstProductItemNameFromCart() {
-        return $x("//div[@class='cart-product__main']/child::a[@class='cart-product__title']").getText();
+    public String getProductItemNameFromCartByNumber(int productNumber) {
+        return $x(format("(//div[@class='cart-product__main']/child::a[@class='cart-product__title'])[%d]", productNumber)).getText();
     }
 }
