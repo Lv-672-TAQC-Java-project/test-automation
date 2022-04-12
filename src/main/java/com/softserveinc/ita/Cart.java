2c93a0ab04138f137711253c8e26d52b4b28a9a0
Kateryna Mushynska <kate080227@gmail.com>
2022-04-12 06:58:07 +0300

    kmushynska: fixed

--- src/main/java/com/softserveinc/ita/Cart.java
+++ src/main/java/com/softserveinc/ita/Cart.java
@@ -1,34 +1,18 @@
 package com.softserveinc.ita;
 
-import java.time.Duration;
-
-import static com.codeborne.selenide.CollectionCondition.sizeNotEqual;
-import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class Cart {
 
-    private String productInCartPath = "//ul[@class='cart-list ng-star-inserted']/li";
-
-    public boolean isCartContainsAddedCountOfProduct(int countOfAddedProduct) {
-        int numberOfProductInCart = $$x(productInCartPath)
-                .shouldBe(sizeNotEqual(0), Duration.ofSeconds(20)).size();
-        if (numberOfProductInCart == countOfAddedProduct){
-            return true;
-        }
-        return false;
-    }
-
-    public Cart openCartProductActions(){
-        $x("//button[@id='cartProductActions0']").click();
+    public Cart openProductActionsInCart(String productName){
+        $x(String.format("//a[@title='%s']/parent::div/following-sibling::rz-popup-menu/button",productName)).click();
 
         return this;
     }
 
-    public Cart removeOption(){
-        $x("//button[contains(text(),'Видалити')]").click();
+    public Cart removeProductItemFromCart(){
+        $x("//li[@class='popup-menu__item ng-star-inserted']//button").click();
 
         return this;
     }
@@ -36,8 +20,7 @@ public class Cart {
     public boolean isCartEmpty(){
         String cartHeadingPath = "//div[@data-testid='empty-cart']/h4";
 
-        return $x(cartHeadingPath).shouldHave(text("Кошик порожній"))
-                .shouldBe(visible)
+        return $x(cartHeadingPath).shouldBe(visible)
                 .isDisplayed();
     }
 }
