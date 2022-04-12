e79160fa658b2c8354e30131dc3aa9ff91750f8d
Kateryna Mushynska <kate080227@gmail.com>
2022-04-12 11:18:09 +0300

    kmushynska: added InCartProduct class

--- src/main/java/com/softserveinc/ita/Cart.java
+++ src/main/java/com/softserveinc/ita/Cart.java
@@ -1,5 +1,9 @@
 package com.softserveinc.ita;
 
 public class Cart {
-    // to be extended
+
+    public InCartProduct getProduct(int index) {
+
+        return new InCartProduct(String.format("(//div[@class='cart-product ng-star-inserted'])[%s]", index));
+    }
 }
