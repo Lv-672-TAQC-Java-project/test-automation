8a0540f7fdeed7de56d3f0d6ba89e382ad818463
yurii <yuriidru23@gmail.com>
2022-04-13 09:41:52 +0300

    ydruzhytskyi: removed unneeded method

--- src/main/java/com/softserveinc/ita/pageobjects/Cart.java
+++ src/main/java/com/softserveinc/ita/pageobjects/Cart.java
@@ -1,16 +1,11 @@
 package com.softserveinc.ita.pageobjects;
 
-import static com.codeborne.selenide.Selenide.$x;
 import static java.lang.String.format;
 
 public class Cart {
 
     public InCartProduct getProduct(int index) {
 
-        return new InCartProduct(format("(//div[@class='cart-product ng-star-inserted'])[%s]", index));
-    }
-
-    public String getProductName(int productNumber) {
-        return $x(format("(//div[@class='cart-product__main']/child::a[@class='cart-product__title'])[%d]", productNumber)).getText();
+        return new InCartProduct(String.format("(//div[@class='cart-product ng-star-inserted'])[%s]", index));
     }
 }
