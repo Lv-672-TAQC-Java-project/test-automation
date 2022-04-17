9ba3b9695396c553dd168bd31965f00d92241b66
mikeromanyshyn <mikesunry@gmail.com>
2022-04-18 01:09:54 +0300

    mromanyshyn: Edited openSubCategory() method

--- src/main/java/com/softserveinc/ita/pageobjects/Catalog.java
+++ src/main/java/com/softserveinc/ita/pageobjects/Catalog.java
@@ -7,8 +7,8 @@ import static com.codeborne.selenide.Selenide.$x;
 public class Catalog {
     @Step("Hovered \"{categoryName}\" Category and opened \"{subCategoryName}\" Sub Category")
     public SearchResultPage openSubCategory(String categoryName, String subCategoryName) {
-        $x(String.format("//ul[@class='menu-categories ng-star-inserted']//*[text()[contains(.,'%s')]]", categoryName))
-                .hover();
+        $x(String.format("//ul[@class='menu-categories ng-star-inserted']//*[text()[contains(.,'%s')]]",
+                categoryName)).hover();
         $x(String.format("//a[@class='menu__link' and text()[contains(.,'%s')]]", subCategoryName)).click();
 
         return new SearchResultPage();
