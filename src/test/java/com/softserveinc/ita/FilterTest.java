d1bc8c4837cd31f37fb6481dcb96978a5b165be9
Veglok <Qwertywo2000@gmail.com>
2022-04-29 09:40:15 +0300

    refactored code, deleted redundant test class

--- src/test/java/com/softserveinc/ita/FilterTest.java
+++ src/test/java/com/softserveinc/ita/FilterTest.java
@@ -1,9 +1,11 @@
 package com.softserveinc.ita;
 
+import com.softserveinc.ita.pageobjects.SearchResultPage;
 import com.softserveinc.ita.pageobjects.product.Product;
 import com.softserveinc.ita.utils.TestRunner;
 import io.qameta.allure.Description;
 import io.qameta.allure.Issue;
+import org.assertj.core.api.Assertions;
 import org.testng.annotations.Test;
 
 import java.util.List;
@@ -27,5 +29,28 @@ public class FilterTest extends TestRunner {
         filteredProductsList.forEach(product -> assertThat(product.getName())
                 .as("Product name should contains " + expectedTerm)
                 .containsIgnoringCase(expectedTerm));
+    }
+
+    @Description("Verify filtering products by custom price functionality")
+    @Issue("https://jira.softserve.academy/browse/LVTAQC672-7")
+    @Test(description = "LVTAQC672-7")
+    public void verifyCustomPriceFilteringFunctionality() {
+        String searchTerm = "Asus";
+        int priceRangeMinimum = 2000;
+        int priceRangeMaximum = 24000;
+
+        SearchResultPage searchResultPage = homePage
+                .getHeader()
+                .search(searchTerm)
+                .getFilter()
+                .filterByPrice(priceRangeMinimum, priceRangeMaximum);
+
+        List<Product> products = searchResultPage.getProducts();
+        List<Integer> pricesList = searchResultPage.getProductsPrices(products);
+
+        pricesList.forEach(productPrice -> Assertions.assertThat(productPrice)
+                .as("products prices should not exceed or be lower than custom price range")
+                .isGreaterThanOrEqualTo(priceRangeMinimum)
+                .isLessThanOrEqualTo(priceRangeMaximum));
     }
 }
