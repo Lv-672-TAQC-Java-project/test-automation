761e3b598a9c5bf0438ad74e73aca47bdc70f2e1
yurii <yuriidru23@gmail.com>
2022-05-05 22:23:51 +0300

    ydruzhytskyi: added getProductCharacteristics method

--- src/main/java/com/softserveinc/ita/pageobjects/CharacteristicTab.java
+++ src/main/java/com/softserveinc/ita/pageobjects/CharacteristicTab.java
@@ -1,5 +1,32 @@
 package com.softserveinc.ita.pageobjects;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+import static java.lang.String.format;
+
 public class CharacteristicTab {
-    // to be extended
+
+    public Map<String, String> getProductCharacteristics() {
+        var productCharacteristicsMap = new HashMap<String, String>();
+        var characteristicSectionsPath = "//section[@class='characteristics-full__group ng-star-inserted']";
+        var sections = $$x(characteristicSectionsPath);
+
+        for (int i = 1; i <= sections.size(); i++) {
+            var characteristicParametersPath = format("%s[%d]%s", characteristicSectionsPath, i, "//div[@class='characteristics-full__item ng-star-inserted']");
+            var characteristicParameters = $$x(characteristicParametersPath);
+
+            for (int a = 1; a <= characteristicParameters.size(); a++) {
+                var characteristicParameterPath = format("%s[%d]", characteristicParametersPath, a);
+                var name = $x(characteristicParameterPath + "/dt/span").getText();
+                var value = $x(characteristicParameterPath + "/descendant::li/*").getText();
+
+                productCharacteristicsMap.put(name, value);
+            }
+        }
+
+        return productCharacteristicsMap;
+    }
 }
