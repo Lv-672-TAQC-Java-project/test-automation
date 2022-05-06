4fc02523c24ccc61d6785e0cceef4c631b832505
yurii <yuriidru23@gmail.com>
2022-05-06 11:16:05 +0300

    ydruzhytskyi: edited getProductCharacteristics method

--- src/main/java/com/softserveinc/ita/pageobjects/CharacteristicTab.java
+++ src/main/java/com/softserveinc/ita/pageobjects/CharacteristicTab.java
@@ -4,27 +4,23 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.$$x;
-import static com.codeborne.selenide.Selenide.$x;
-import static java.lang.String.format;
 
 public class CharacteristicTab {
 
     public Map<String, String> getProductCharacteristics() {
         var productCharacteristicsMap = new HashMap<String, String>();
-        var characteristicSectionsPath = "//section[@class='characteristics-full__group ng-star-inserted']";
-        var sections = $$x(characteristicSectionsPath);
+        var sections = $$x("//section[@class='characteristics-full__group ng-star-inserted']");
 
-        for (int i = 1; i <= sections.size(); i++) {
-            var characteristicParametersPath = format("%s[%d]%s", characteristicSectionsPath, i, "//div[@class='characteristics-full__item ng-star-inserted']");
-            var characteristicParameters = $$x(characteristicParametersPath);
+        for (var section : sections) {
+            var parameters = section.$$x(".//div[@class='characteristics-full__item ng-star-inserted']");
+            parameters
+                    .asFixedIterable()
+                    .forEach(parameter -> {
+                        var name = parameter.$x("./dt/span").getText();
+                        var value = parameter.$x("./descendant::li/*").getText();
 
-            for (int a = 1; a <= characteristicParameters.size(); a++) {
-                var characteristicParameterPath = format("%s[%d]", characteristicParametersPath, a);
-                var name = $x(characteristicParameterPath + "/dt/span").getText();
-                var value = $x(characteristicParameterPath + "/descendant::li/*").getText();
-
-                productCharacteristicsMap.put(name, value);
-            }
+                        productCharacteristicsMap.put(name, value);
+                    });
         }
 
         return productCharacteristicsMap;
