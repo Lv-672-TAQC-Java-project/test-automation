42312b53b928813c2a6d9023197352373179c71a
yurii <yuriidru23@gmail.com>
2022-05-09 10:14:19 +0300

    ydruzhytskyi: refactored getProductCharacteristics method

--- src/main/java/com/softserveinc/ita/pageobjects/CharacteristicTab.java
+++ src/main/java/com/softserveinc/ita/pageobjects/CharacteristicTab.java
@@ -4,23 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+import static java.lang.String.format;
 
 public class CharacteristicTab {
 
     public Map<String, String> getProductCharacteristics() {
         var productCharacteristicsMap = new HashMap<String, String>();
-        var sections = $$x("//section[@class='characteristics-full__group ng-star-inserted']");
+        var characteristicSectionsPath = "//section[@class='characteristics-full__group ng-star-inserted']";
+        var sections = $$x(characteristicSectionsPath);
 
-        for (var section : sections) {
-            var parameters = section.$$x(".//div[@class='characteristics-full__item ng-star-inserted']");
-            parameters
-                    .asFixedIterable()
-                    .forEach(parameter -> {
-                        var name = parameter.$x("./dt/span").getText();
-                        var value = parameter.$x("./descendant::li/*").getText();
+        for (int sectionNumber = 1; sectionNumber <= sections.size(); sectionNumber++) {
+            var characteristicSectionPath = format("%s[%d]", characteristicSectionsPath, sectionNumber);
+            var parametersRowPath = "//div[@class='characteristics-full__item ng-star-inserted']";
+            var characteristicParametersPath = characteristicSectionPath + parametersRowPath;
+            var characteristicParameters = $$x(characteristicParametersPath);
 
-                        productCharacteristicsMap.put(name, value);
-                    });
+            for (int parameterNumber = 1; parameterNumber <= characteristicParameters.size(); parameterNumber++) {
+                var characteristicParameterPath = format("%s[%d]", characteristicParametersPath, parameterNumber);
+                var name = $x(characteristicParameterPath + "/dt/span").getText();
+                var value = $x(characteristicParameterPath + "/descendant::li/*").getText();
+
+                productCharacteristicsMap.put(name, value);
+            }
         }
 
         return productCharacteristicsMap;
